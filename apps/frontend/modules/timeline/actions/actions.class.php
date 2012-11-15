e4fbce4608d94bdea98283f94c730e4b50f294ad
poulping <noar99@hotmail.com>
2012-11-15 17:10:38 -0500

    dev access config

--- apps/frontend/modules/timeline/actions/actions.class.php
+++ apps/frontend/modules/timeline/actions/actions.class.php
@@ -10,13 +10,23 @@
  */
 class timelineActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->forward('default', 'module');
-  }
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeIndex(sfWebRequest $request)
+    {
+        //$this->forward('default', 'module');
+    }
+
+    public function executeAdd(sfWebRequest $request)
+    {
+        //$this->forward('default', 'module');
+    }
+
+    public function executeShow(sfWebRequest $request)
+    {
+        //$this->forward('default', 'module');
+    }
 }
