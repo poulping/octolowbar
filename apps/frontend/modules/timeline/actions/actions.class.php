9eea6dd8125d0c7bc3d1fcd7dc0d06e2405831a4
poulping <noar99@hotmail.com>
2012-11-15 16:54:34 -0500

    timeline module

---
+++ apps/frontend/modules/timeline/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * timeline actions.
+ *
+ * @package    octolowbar
+ * @subpackage timeline
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class timelineActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
