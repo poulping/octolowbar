aa77974f33d87e93516cf6acbbba41f474820ddd
MathRivest <rivest.mathieu@gmail.com>
2012-11-15 18:32:48 -0500

    added some css

--- apps/frontend/modules/timeline/templates/indexSuccess.php
+++ apps/frontend/modules/timeline/templates/indexSuccess.php
@@ -1,11 +1,19 @@
 BONJOUR
 
 <ul id="timeline">
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
-    <li class="m-row">Item</li>
+    <li class="m-row">
+        <a href="#" class="m-item">
+            Item 1
+        </a>
+    </li>
+    <li class="m-row">
+        <a href="#" class="m-item">
+            Item 2
+        </a>
+    </li>
+    <li class="m-row">
+        <a href="#" class="m-item">
+            Item 3
+        </a>
+    </li>
 </ul>
