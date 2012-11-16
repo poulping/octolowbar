0877bc2d8114d9bdf93f15f215b5b1d87e1569d1
MathRivest <rivest.mathieu@gmail.com>
2012-11-16 17:23:02 -0500

    added style for items

--- apps/frontend/modules/timeline/templates/indexSuccess.php
+++ apps/frontend/modules/timeline/templates/indexSuccess.php
@@ -1,19 +1,24 @@
-BONJOUR
+OCTOLOWBAR
 
 <ul id="timeline">
-    <li class="m-row">
-        <a href="#" class="m-item">
-            Item 1
+    <li class="m-row" data-timestamp="">
+        <a href="#" class="m-item l-video">
+            <iframe width="560" height="315" src="http://www.youtube.com/embed/sQuTrTd0JgI?rel=0" frameborder="0" allowfullscreen></iframe>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lectus tortor, tincidunt ornare pharetra sed, rhoncus sed lectus. Donec elit ante, lobortis a mattis sed, consectetur in risus. Donec nec lacinia risus. Proin neque sapien, tincidunt id semper et, fermentum in tortor. Fusce mattis luctus tellus non elementum. Aenean non mi leo. Duis tempus fringilla pulvinar. Morbi ut vulputate augue. Cras viverra, lacus vitae sollicitudin tincidunt, magna felis cursus lectus, eget aliquet velit turpis ut est.</p>
+            <div class="name">Arnaud</div>
         </a>
     </li>
-    <li class="m-row">
-        <a href="#" class="m-item">
-            Item 2
+    <li class="m-row" data-timestamp="">
+        <a href="#" class="m-item l-photo">
+            <img src="http://placekitten.com/560/315" width="560" height="315"/>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lectus tortor, tincidunt ornare pharetra sed, rhoncus sed lectus. Donec elit ante, lobortis a mattis sed, consectetur in risus. Donec nec lacinia risus. Proin neque sapien, tincidunt id semper et, fermentum in tortor. Fusce mattis luctus tellus non elementum. Aenean non mi leo. Duis tempus fringilla pulvinar. Morbi ut vulputate augue. Cras viverra, lacus vitae sollicitudin tincidunt, magna felis cursus lectus, eget aliquet velit turpis ut est.</p>
+            <div class="name">Mathieu</div>
         </a>
     </li>
-    <li class="m-row">
-        <a href="#" class="m-item">
-            Item 3
+    <li class="m-row" data-timestamp="">
+        <a href="#" class="m-item l-text">
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lectus tortor, tincidunt ornare pharetra sed, rhoncus sed lectus. Donec elit ante, lobortis a mattis sed, consectetur in risus. Donec nec lacinia risus. Proin neque sapien, tincidunt id semper et, fermentum in tortor. Fusce mattis luctus tellus non elementum. Aenean non mi leo. Duis tempus fringilla pulvinar. Morbi ut vulputate augue. Cras viverra, lacus vitae sollicitudin tincidunt, magna felis cursus lectus, eget aliquet velit turpis ut est.</p>
+            <div class="name">Alex</div>
         </a>
     </li>
 </ul>
