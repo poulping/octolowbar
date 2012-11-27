ab1fc752ae702e5e58e3bb888ba323264adc2a92
poulping <noar99@hotmail.com>
2012-11-27 18:24:48 -0500

    add ajax ids in sessions

--- apps/frontend/modules/timeline/actions/actions.class.php
+++ apps/frontend/modules/timeline/actions/actions.class.php
@@ -146,6 +146,7 @@ class timelineActions extends sfActions
 
         if ($new_content)
         {
+            $this->getUser()->setKeysSent(array($new_content->getPrimaryKey()));
             return $this->getPartial('contentContainer', array('content' => $new_content, 'col' => false));
         }
         return $this->renderText('0');
